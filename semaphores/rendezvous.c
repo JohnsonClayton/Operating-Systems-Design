460385266b25c572deedc032aa4298a49a367530
Clayton Johnson <cpjohnson@mavs.coloradomesa.edu>
2019-04-16 13:00:03 -0600

    Solved rendezvous problem

--- semaphores/rendezvous.c
+++ semaphores/rendezvous.c
@@ -2,30 +2,40 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-sem_t sem;
+sem_t first_stmt, second_stmt, third_stmt;
 
 void *statement1(void *args) {
-	printf("statement1 reached\n");
-	while(1) {
-		printf("1\n");
-	}
+	printf("1\n");
+	sem_post(&first_stmt);
+	sem_wait(&third_stmt);
+	sem_wait(&second_stmt);
+	printf("3\n");
+	sem_post(&second_stmt);
+	sem_post(&third_stmt);
 	return NULL;
 }
 
 void *statement2(void *args) {
-	printf("statement2 reached\n");
-	while(1) {
-		printf("2\n");
-	}
+	printf("2\n");
+	sem_post(&first_stmt);
+	sem_post(&second_stmt);
+	sem_wait(&third_stmt);
+	printf("4\n");
+	sem_post(&third_stmt);
 	return NULL;
 }
 
 int main(int argc, char *argv[]) {
 	pthread_t a, b;
 
-	sem_init(&sem, 0, 1);
+	sem_init(&first_stmt, 0, 1);
+	sem_init(&second_stmt, 0, 1);
+	sem_init(&third_stmt, 0, 1);
 
+	sem_wait(&second_stmt);
+	sem_wait(&first_stmt);
 	pthread_create(&a, NULL, statement1, NULL);
+	sem_wait(&first_stmt);
 	pthread_create(&b, NULL, statement2, NULL);
 
 	pthread_join(a, NULL);
